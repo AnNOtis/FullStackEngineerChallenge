1f7d5a81782584c6b470ce73a7466f8ce7de1c82
Otis Chou <altispec@gmail.com>
2019-10-02 00:41:16 +0800

    feat(server): Add authentication

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
+  rescue_from Errors::APIError, :with => :render_errors
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id])
+    else
+      @current_user = nil
+    end
+  end
+
+  private
+
+  def render_errors exception
+    render json: exception, status: exception.status
+  end
 end
---
+++ server/app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+  def auth
+    if current_user
+      render_user current_user
+    else
+      raise Errors::UnauthorizedError
+    end
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      render_user user
+    else
+      raise Errors::UnauthorizedError, 'email or password is invalid'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    render :nothing, status: 200
+  end
+
+  private
+
+  def render_user user
+    render json: { user: user.as_json(only: [:email, :name, :is_admin]) }
+  end
+end
