e0d83ad472a94e1f31ee7c3aa48601bf150f14b2
Otis Chou <altispec@gmail.com>
2019-10-05 00:09:37 +0800

    feat(client): add different color to admin page's header

--- client/src/components/MaterialThemeProvider.js
+++ client/src/components/MaterialThemeProvider.js
@@ -1,15 +1,25 @@
 import React from 'react'
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider as StyledThemeProvider } from 'styled-components'
 import PropTypes from 'prop-types'
 import NoSsr from '@material-ui/core/NoSsr'
 import { createMuiTheme } from '@material-ui/core/styles'
+import { blueGrey } from '@material-ui/core/colors'
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/styles'
 
-const defaultTheme = createMuiTheme()
+const theme = createMuiTheme({
+  palette: {
+    secondary: {
+      main: blueGrey[800]
+    }
+  }
+})
 
 function MaterialThemeProvider({ children }) {
   return (
     <NoSsr>
-      <ThemeProvider theme={defaultTheme}>{children}</ThemeProvider>
+      <MuiThemeProvider theme={theme}>
+        <StyledThemeProvider theme={theme}>{children}</StyledThemeProvider>
+      </MuiThemeProvider>
     </NoSsr>
   )
 }
--- client/src/components/layouts/MainLayout/index.js
+++ client/src/components/layouts/MainLayout/index.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
-import MenuIcon from '@material-ui/icons/Menu'
+import { useRouteMatch } from 'react-router-dom'
+import { Menu as MenuIcon, Lock as LockIcon } from '@material-ui/icons'
 import {
   AppBar,
   Typography,
   Toolbar,
   Container,
   Box,
-  IconButton
+  IconButton,
+  Chip
 } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import Sidebar from '@/components/Sidebar'
@@ -20,12 +22,13 @@ const useStyles = makeStyles({
 })
 
 function MainLayout({ children }) {
+  let isAdminPage = useRouteMatch('/admin')
   const [isSidebarOpen, setSidebar] = useState(false)
   const classes = useStyles()
 
   return (
     <div>
-      <AppBar position="static">
+      <AppBar position="static" color={isAdminPage ? 'secondary' : 'primary'}>
         <Toolbar>
           <IconButton
             edge="start"
@@ -38,7 +41,14 @@ function MainLayout({ children }) {
             <MenuIcon />
           </IconButton>
           <Typography className={classes.title} variant="h6">
-            Performance Review
+            Performance Review{' '}
+            {isAdminPage && (
+              <Chip
+                icon={<LockIcon />}
+                label="Admin console"
+                color="secondary"
+              />
+            )}
           </Typography>
           <UserButton />
         </Toolbar>
